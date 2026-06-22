96419a21a70d62b017fd82f9f5cfb4d129754d64
cybereun <cybereunny@gmail.com>
2026-06-22 21:03:42 +0900

    fix: disable GPU and sandbox to resolve Electron crash

--- src/main.js
+++ src/main.js
@@ -121,6 +121,12 @@ function registerGlobalShortcuts() {
 }
 
 app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('no-sandbox');
+app.commandLine.appendSwitch('disable-gpu');
+app.commandLine.appendSwitch('disable-software-rasterizer');
+app.commandLine.appendSwitch('disable-gpu-compositing');
+app.commandLine.appendSwitch('disable-gpu-rasterization');
+app.commandLine.appendSwitch('disable-gpu-sandbox');
 
 const WINDOW_SIZE = {
   collapsed: { width: 290, height: 230 },
